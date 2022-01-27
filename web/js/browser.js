61d62325ff1ec75b7e2bce98a2da84a558400c8f
Luiz Filipy <luizfilipy.aa@outlook.com>
2022-01-27 17:59:55 -0300

    Update web page

---
+++ web/js/browser.js
@@ -0,0 +1,29 @@
+(function () {
+    window.addEventListener('resize', event => {
+        showDisplaySize();
+    }, true);
+
+    selectAll('botcity')
+})();
+
+function selectAll(id) {
+    const element = document.getElementById(id);
+    if (document.body.createTextRange) {
+        const range = document.body.createTextRange();
+        range.moveToElementText(element);
+        range.select();
+    } else if (window.getSelection) {
+        const selection = window.getSelection();
+        const range = document.createRange();
+        range.selectNodeContents(element);
+        selection.removeAllRanges();
+        selection.addRange(range);
+    } else {
+        console.warn("Could not select text in node: Unsupported browser.");
+    }
+}
+
+function showDisplaySize() {
+    document.getElementById('screen-size').innerText = `${window.screen.width}x${window.screen.height}`;
+    document.getElementById('page-size').innerText = `${window.innerWidth}x${window.innerHeight}`;
+}
