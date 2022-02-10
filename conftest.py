05723431c15882bc43af5da0403e2f13c6099b81
Luiz Filipy <luizfilipy.aa@outlook.com>
2022-02-10 17:08:14 -0300

    update

--- conftest.py
+++ conftest.py
@@ -16,20 +16,32 @@ def web(request):
     web.headless = request.config.getoption("--headless")
     web.browser = request.config.getoption("--browser") or Browser.CHROME
 
+    plt = platform.system()
+
     if web.browser == 'edge':
-        if platform.system() == 'Linux':
+        if plt == 'Linux':
             opt = edge.default_options()
             opt.add_argument('--remote-debugging-port=9222')
             opt.add_argument('--no-sandbox')
             opt.add_argument('--disable-dev-shm-usage')
             opt.set_capability('platform', 'LINUX')  # WINDOWS is default value:
             web.options = opt
+            web.driver_path = os.path.join(project_path, 'web-drivers', 'msedgedriver')
+        elif plt == 'Windows':
+            web.driver_path = os.path.join(project_path, 'web-drivers', 'msedgedriver.exe')
 
-        web.driver_path = os.path.join(project_path, 'web-drivers', 'msedgedriver')
     elif web.browser == 'firefox':
-        web.driver_path = os.path.join(project_path, 'web-drivers', 'geckodriver')
+        if plt == 'Linux':
+            web.driver_path = os.path.join(project_path, 'web-drivers', 'geckodriver')
+        elif plt == 'Windows':
+            web.driver_path = os.path.join(project_path, 'web-drivers', 'geckodriver.exe')
+
     else:
-        web.driver_path = os.path.join(project_path, 'web-drivers', 'chromedriver')
+        if plt == 'Linux':
+            web.driver_path = os.path.join(project_path, 'web-drivers', 'chromedriver')
+        elif plt == 'Windows':
+            web.driver_path = os.path.join(project_path, 'web-drivers', 'chromedriver.exe')
+
     yield web
 
     web.stop_browser()
