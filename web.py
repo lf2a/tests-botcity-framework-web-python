aa43fe1ab3f464d9086e5af337d075172b8d1fde
Luiz Filipy <luizfilipy.aa@outlook.com>
2022-02-10 13:49:04 -0300

    update

--- web.py
+++ web.py
@@ -1,4 +1,22 @@
 import os
 
+from botcity.web import WebBot, Browser, browsers
+
 print(os.path.abspath(''))
-print(os.path.exists(os.path.join(os.path.abspath(''), 'web-drivers', 'chromedriver')))
+print(os.path.exists(os.path.join(os.path.abspath(''), 'web-drivers', 'msedgedriver')))
+
+web = WebBot()
+web.headless = False
+web.browser = Browser.EDGE
+web.driver_path = os.path.join(os.path.abspath(''), 'web-drivers', 'msedgedriver')
+
+opt = browsers.edge.default_options()
+opt.add_argument('--remote-debugging-port=9222')
+opt.add_argument('--no-sandbox')
+opt.add_argument('--disable-dev-shm-usage')
+opt.set_capability('platform', 'LINUX')  # WINDOWS is default value:
+web.options = opt
+
+web.browse('https://google.com')
+web.wait(5_000)
+web.stop_browser()
